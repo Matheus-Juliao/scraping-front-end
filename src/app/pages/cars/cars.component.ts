79733793c4f3b12052bee083506f32fd76bd3cdc
Matheus.Juliao <matheus.juliao@stoneridge.com>
2022-10-27 18:55:48 -0300

    dev reference

--- src/app/pages/cars/cars.component.ts
+++ src/app/pages/cars/cars.component.ts
@@ -1,7 +1,11 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import dbBrand from 'src/assets/brand.json';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PagesService } from '../pages.service';
+
+//Json
+import dbBrand from 'src/assets/brand.json';
+import dbReference from 'src/assets/reference.json';
+
 
 @Component({
   selector: 'app-cars',
@@ -11,6 +15,7 @@ import { PagesService } from '../pages.service';
 export class CarsComponent implements OnInit {
 
   public brands: { Label: string, Value: string }[] = dbBrand;
+  public reference: { Codigo: number, Mes: string, Index: number }[] = dbReference;
 
   private payload: any = { 
     brand: undefined, 
@@ -30,16 +35,35 @@ export class CarsComponent implements OnInit {
   public dbModels: any
   public models: any
   public years: any
+  public firstPeriod: any
+  public finalPeriod: any
 
   constructor(private pagesService: PagesService) { }
 
   ngOnInit(): void {
     //
+    console.log(this.reference)
+
   }
 
   public onSubmit(form: NgForm) {
     this.payload = form.value
     this.showLoanding = true
+    console.log(form.value)
+
+    //   {
+    //     "brand": "207",
+    //     "model": "8275",
+    //     "year": "1985-1",
+    //     "initialReference": "290", //Index: 262
+    //     "finalReference": "288"  //Index: 260
+    //     "period": "290" //Index: /262
+    // }
+
+    // const iPeriod = this.payload.Index
+    // const fPeriod = this.payload.
+
+
     this.pagesService.postForm(this.payload).subscribe({
       next: (res: any) => {
         this.results = res
